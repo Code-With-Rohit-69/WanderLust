d89db1934285bd5f5868e56405feac021153159a
Rohit Singh <rr2488017@gmail.com>
2024-06-16 11:39:04 +0530

    'commited'

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require('./Routes/listing.js');
-const reviews = require('./Routes/review.js');
+const listingRouter = require('./Routes/listing.js');
+const reviewRouter = require('./Routes/review.js');
+const userRouter = require("./Routes/user.js");
 const session = require('express-session');
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require('./models/user.js');
 
 const app = express();
 
@@ -51,6 +55,13 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -58,8 +69,19 @@ app.use((req, res, next) => {
   next();
 })
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get('/demouser', async (req, res) => {
+//   let fakeUser = new User({
+//     email: 'student@gmail.com',
+//     username: 'Thakur'
+//   })
+
+//   let registeredUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+// })
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use('/', userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not Found"));
